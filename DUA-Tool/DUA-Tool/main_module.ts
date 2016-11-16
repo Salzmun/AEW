9ab7ae255e5bd7c86e5b546236dafcf8a80821e6
Max Salzmann <salzmun@gmail.com>
2016-11-16 14:19:44 +0100

    V0.2.2 - Minor Changes

--- DUA-Tool/DUA-Tool/main_module.ts
+++ DUA-Tool/DUA-Tool/main_module.ts
@@ -22,32 +22,13 @@
 
         }
 
+        function editStuff() {
+        }
+
+        function newPage() {
+
+        }
+
         function draganddrop() {
 
         }
-
-module Graphic{
-
-    var exporter:Main;
-
-    export function start() {
-        if (!exporter)
-            exporter = new Main();
-            } 
-
-            class Main{
-
-
-            
-                constructor() {
-
-                }
-
-
-
-
-            }
-
-        }
-if ($)
-    $(document).ready(Graphic.start);
