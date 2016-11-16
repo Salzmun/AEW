28be5e0250fe83751cef56a41bc0329eebc6070f
Max Salzmann <salzmun@gmail.com>
2016-11-16 12:00:26 +0100

    Vers 0.2 -  TS Main Module

--- DUA-Tool/DUA-Tool/main_module.js
+++ DUA-Tool/DUA-Tool/main_module.js
@@ -1,1 +1,31 @@
+function addNode() {
+}
+function deleteNode() {
+}
+function createConnector() {
+}
+function openFile() {
+}
+function saveFile() {
+}
+function labelStuff() {
+}
+function draganddrop() {
+}
+var Graphic;
+(function (Graphic) {
+    var exporter;
+    function start() {
+        if (!exporter)
+            exporter = new Main();
+    }
+    Graphic.start = start;
+    var Main = (function () {
+        function Main() {
+        }
+        return Main;
+    }());
+})(Graphic || (Graphic = {}));
+if ($)
+    $(document).ready(Graphic.start);
 //# sourceMappingURL=main_module.js.map
--- DUA-Tool/DUA-Tool/main_module.ts
+++ DUA-Tool/DUA-Tool/main_module.ts
@@ -1,1 +1,53 @@
-﻿
+﻿        function addNode() {
+
+        }
+
+        function deleteNode() {
+
+        }
+
+        function createConnector() {
+
+        }
+
+        function openFile() {
+
+        }
+
+        function saveFile() {
+
+        }
+
+        function labelStuff() {
+
+        }
+
+        function draganddrop() {
+
+        }
+
+module Graphic{
+
+    var exporter:Main;
+
+    export function start() {
+        if (!exporter)
+            exporter = new Main();
+            } 
+
+            class Main{
+
+
+            
+                constructor() {
+
+                }
+
+
+
+
+            }
+
+        }
+if ($)
+    $(document).ready(Graphic.start);
