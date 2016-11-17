86dcd0278ee0d99a514e6ae490e5b2d3c0ea72fc
Moritz <mowieg@gmail.com>
2016-11-17 12:37:04 +0100

    Version 0.3.4

--- DUA-Tool/DUA-Tool/app.js
+++ DUA-Tool/DUA-Tool/app.js
@@ -1,28 +1,4 @@
 var id = 0;
-jsPlumb.ready(function (e) {
-    jsPlumb.setContainer($('#canvas'));
-    $(".cbtn_add").draggable({
-        helper: 'clone',
-        cursor: 'pointer',
-        tolerance: 'fit',
-        revert: true
-    });
-    $("#canvas").droppable({
-        accept: '.toolbox',
-        containment: 'canvas',
-        drop: function (e, ui) {
-            var droppedElement = ui.helper.clone();
-            ui.helper.remove();
-            $(droppedElement).removeAttr("class");
-            jsPlumb.repaint(ui.helper);
-            $(droppedElement).addClass("node_tree");
-            $(droppedElement).draggable({
-                containment: "canvas"
-            });
-            droppedElement.appendTo('#canvas');
-        }
-    });
-});
 function imageClick(url) {
     window.location = url;
 }
@@ -32,16 +8,16 @@ function hover(x) {
 function hoverout(x) {
     x.style.opacity = "0.5";
 }
-function click_open() {
+function click_addNode() {
     //$("#fileLoader").click();
     id++;
     var endpointOptions = { isSource: false, isTarget: true };
     var endpointOptions1 = { isSource: true, isTarget: false, maxConnections: -1, connector: ["Straight"] };
-    var structure = $('<div class="node_p" id="' + id + '" > </div>');
+    var structure = $('<div class="node_tree" id="' + id + '" > </div>');
     $('#canvas').append(structure);
     jsPlumb.addEndpoint(id.toString(), { anchor: "Top" }, endpointOptions);
     jsPlumb.addEndpoint(id.toString(), { anchor: "Bottom" }, endpointOptions1);
-    jsPlumb.draggable($(".node_p"));
+    jsPlumb.draggable($(".node_tree"));
     //jsPlumb.connect({ source: "1", target: "2" });
 }
 function treeClick() {
@@ -63,13 +39,13 @@ function hideButtons() {
     $('#site_choice').hide();
 }
 function click_delete() {
-    $('.node_p').bind('click', function () {
+    $('.node_tree').bind('click', function () {
         jsPlumb.detachAllConnections(this.id);
         jsPlumb.removeAllEndpoints(this.id);
         $(this).remove();
     });
     $('.canvas').bind('click', function () {
-        $('.node_p').unbind();
+        $('.node_tree').unbind();
     });
 }
 //# sourceMappingURL=app.js.map
