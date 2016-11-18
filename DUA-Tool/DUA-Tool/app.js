d27eb876c7913cd55d81d3da2f5b885f8a6d7272
Max Salzmann <salzmun@gmail.com>
2016-11-18 09:33:11 +0100

    V0.3.4

--- DUA-Tool/DUA-Tool/app.js
+++ DUA-Tool/DUA-Tool/app.js
@@ -1,51 +1,112 @@
-var id = 0;
-function imageClick(url) {
-    window.location = url;
-}
-function hover(x) {
-    x.style.opacity = "1.0";
-}
-function hoverout(x) {
-    x.style.opacity = "0.5";
-}
-function click_addNode() {
-    //$("#fileLoader").click();
-    id++;
-    var endpointOptions = { isSource: false, isTarget: true };
-    var endpointOptions1 = { isSource: true, isTarget: false, maxConnections: -1, connector: ["Straight"] };
-    var structure = $('<div class="node_tree" id="' + id + '" > </div>');
-    $('#canvas').append(structure);
-    jsPlumb.addEndpoint(id.toString(), { anchor: "Top" }, endpointOptions);
-    jsPlumb.addEndpoint(id.toString(), { anchor: "Bottom" }, endpointOptions1);
-    jsPlumb.draggable($(".node_tree"));
-    //jsPlumb.connect({ source: "1", target: "2" });
-}
-function treeClick() {
-    hideButtons();
-    showDrawing();
-}
-function graphClick() {
-    hideButtons();
-    showDrawing();
-}
-function listClick() {
-    hideButtons();
-    showDrawing();
-}
-function showDrawing() {
-    $('#site_drawing').show();
-}
-function hideButtons() {
-    $('#site_choice').hide();
-}
-function click_delete() {
-    $('.node_tree').bind('click', function () {
-        jsPlumb.detachAllConnections(this.id);
-        jsPlumb.removeAllEndpoints(this.id);
-        $(this).remove();
-    });
-    $('.canvas').bind('click', function () {
-        $('.node_tree').unbind();
-    });
-}
+var Graphic;
+(function (Graphic) {
+    var __script = ();
+    function Main() {
+        if (!__script)
+            __script = new DUA();
+    }
+    Graphic.Main = Main;
+    var DUA = (function () {
+        function DUA() {
+            var _this = this;
+            this.imageClick = function (url) {
+                window.location = url;
+            };
+            this.click_addNode = function () {
+                //$("#fileLoader").click();
+                _this._id++;
+                var endpointOptions = { isSource: false, isTarget: true };
+                var endpointOptions1 = { isSource: true, isTarget: false, maxConnections: -1, connector: ["Straight"] };
+                var structure = $('<div class="' + _this._stype + '" id="' + _this._id + '" > </div>');
+                $('#canvas').append(structure);
+                jsPlumb.addEndpoint(id.toString(), { anchor: "Top" }, endpointOptions);
+                jsPlumb.addEndpoint(id.toString(), { anchor: "Bottom" }, endpointOptions1);
+                jsPlumb.draggable($(".node_tree"));
+                //jsPlumb.connect({ source: "1", target: "2" });
+            };
+            this.treeClick = function () {
+                _this.hideButtons();
+                _this.showDrawing();
+                _this._stype = "ntree";
+            };
+            this.graphClick = function () {
+                _this.hideButtons();
+                _this.showDrawing();
+                _this._stype = "ngraph";
+            };
+            this.listClick = function () {
+                _this.hideButtons();
+                _this.showDrawing();
+                _this._stype = "nlist";
+            };
+            this.showDrawing = function () {
+                $('#site_drawing').show();
+            };
+            this.hideButtons = function () {
+                $('#site_choice').hide();
+            };
+            this.click_delete = function () {
+                $('.node_tree').bind('click', function () {
+                    jsPlumb.detachAllConnections(this.id);
+                    jsPlumb.removeAllEndpoints(this.id);
+                    $(this).remove();
+                });
+                $('.canvas').bind('click', function () {
+                    $('.node_tree').unbind();
+                });
+            };
+            this._id = 0;
+        }
+        return DUA;
+    }());
+})(Graphic || (Graphic = {}));
+if ($)
+    $().ready(Graphic.Main);
+//var id = 0;
+//var stype: string;
+//var ncon: string;
+//function imageClick(url) {
+//    window.location = url;
+//}
+//function click_addNode() {
+//    //$("#fileLoader").click();
+//    id++;
+//    var endpointOptions = { isSource: false, isTarget: true };
+//    var endpointOptions1 = { isSource: true, isTarget: false, maxConnections: -1, connector: ["Straight"] };
+//    var structure = $('<div class="node_tree" id="' + id + '" > </div>');
+//    $('#canvas').append(structure);
+//    jsPlumb.addEndpoint(id.toString(), { anchor: "Top" }, endpointOptions);
+//    jsPlumb.addEndpoint(id.toString(), { anchor: "Bottom" }, endpointOptions1);
+//    jsPlumb.draggable($(".node_tree"));
+//    //jsPlumb.connect({ source: "1", target: "2" });
+//}
+//function treeClick(str) {
+//    hideButtons();
+//    showDrawing();
+//    stype = str;
+//}
+//function graphClick(str) {
+//    hideButtons();
+//    showDrawing();
+//}
+//function listClick() {
+//    hideButtons();
+//    showDrawing();
+//}
+//function showDrawing() {
+//    $('#site_drawing').show();
+//}
+//function hideButtons() {
+//    $('#site_choice').hide();
+//}
+//function click_delete() {
+//    $('.node_tree').bind('click', function () {
+//        jsPlumb.detachAllConnections(this.id);
+//        jsPlumb.removeAllEndpoints(this.id);
+//        $(this).remove();
+//    });
+//    $('.canvas').bind('click', function () {
+//        $('.node_tree').unbind();
+//    });
+//}
 //# sourceMappingURL=app.js.map
--- DUA-Tool/DUA-Tool/app.ts
+++ DUA-Tool/DUA-Tool/app.ts
@@ -1,65 +1,144 @@
-﻿var id = 0;
+﻿module Graphic{
 
-function imageClick(url) {
-    window.location = url;
+    var __script: DUA();
+    export function Main() {
+        if (!__script)
+            __script = new DUA();
+
+
+    }
+
+   class DUA {
+        _id: number;
+        _stype: String;
+        _ncon: String;
+
+        constructor() {
+            this._id = 0;
+        }
+
+        imageClick =(url)=> {
+            window.location = url;
+        }
+
+        click_addNode =() => {
+            //$("#fileLoader").click();
+            this._id++;
+            var endpointOptions = { isSource: false, isTarget: true };
+            var endpointOptions1 = { isSource: true, isTarget: false, maxConnections: -1, connector: ["Straight"] };
+
+            var structure = $('<div class="' + this._stype +'" id="' + this._id + '" > </div>');
+            $('#canvas').append(structure);
+            jsPlumb.addEndpoint(id.toString(), { anchor: "Top" }, endpointOptions);
+            jsPlumb.addEndpoint(id.toString(), { anchor: "Bottom" }, endpointOptions1);
+            jsPlumb.draggable($(".node_tree"));
+            //jsPlumb.connect({ source: "1", target: "2" });
+
+        }
+
+        treeClick = () => {
+            this.hideButtons();
+            this.showDrawing();
+            this._stype = "ntree";
+        }
+
+        graphClick = () => {
+            this.hideButtons();
+            this.showDrawing();
+            this._stype = "ngraph";
+        }
+
+        listClick=()=> {
+            this.hideButtons();
+            this.showDrawing();
+            this._stype = "nlist";
+        }
+
+        showDrawing =()=> {
+            $('#site_drawing').show();
+        }
+
+        hideButtons =()=> {
+            $('#site_choice').hide();
+        }
+
+        click_delete=()=> {
+            $('.node_tree').bind('click', function () {
+                jsPlumb.detachAllConnections(this.id);
+                jsPlumb.removeAllEndpoints(this.id);
+                $(this).remove();
+            });
+
+            $('.canvas').bind('click', function () {
+                $('.node_tree').unbind();
+            });
+        }
+
+    }
+
+
 }
 
-function hover(x) {
-    x.style.opacity = "1.0";
-}
+if ($)
+    $().ready(Graphic.Main);
 
-function hoverout(x) {
-    x.style.opacity = "0.5";
-}
+//var id = 0;
+//var stype: string;
+//var ncon: string;
 
-function click_addNode() {
-    //$("#fileLoader").click();
-    id++;
-    var endpointOptions = { isSource: false, isTarget: true };
-    var endpointOptions1 = { isSource: true, isTarget: false, maxConnections: -1, connector: ["Straight"] };
+//function imageClick(url) {
+//    window.location = url;
+//}
 
-    var structure = $('<div class="node_tree" id="' + id + '" > </div>');
-    $('#canvas').append(structure);
-    jsPlumb.addEndpoint(id.toString(), { anchor: "Top" }, endpointOptions);
-    jsPlumb.addEndpoint(id.toString(), { anchor: "Bottom" }, endpointOptions1);
-    jsPlumb.draggable($(".node_tree"));
-    //jsPlumb.connect({ source: "1", target: "2" });
+//function click_addNode() {
+//    //$("#fileLoader").click();
+//    id++;
+//    var endpointOptions = { isSource: false, isTarget: true };
+//    var endpointOptions1 = { isSource: true, isTarget: false, maxConnections: -1, connector: ["Straight"] };
 
-}
+//    var structure = $('<div class="node_tree" id="' + id + '" > </div>');
+//    $('#canvas').append(structure);
+//    jsPlumb.addEndpoint(id.toString(), { anchor: "Top" }, endpointOptions);
+//    jsPlumb.addEndpoint(id.toString(), { anchor: "Bottom" }, endpointOptions1);
+//    jsPlumb.draggable($(".node_tree"));
+//    //jsPlumb.connect({ source: "1", target: "2" });
 
-function treeClick() {
-    hideButtons();
-    showDrawing();
-}
+//}
 
-function graphClick() {
-    hideButtons();
-    showDrawing();
-}
+//function treeClick(str) {
+//    hideButtons();
+//    showDrawing();
+//    stype = str;
+//}
 
-function listClick() {
-    hideButtons();
-    showDrawing();
-}
+//function graphClick(str) {
+//    hideButtons();
+//    showDrawing();
+//}
 
-function showDrawing() {
-    $('#site_drawing').show();
-}
+//function listClick() {
+//    hideButtons();
+//    showDrawing();
+//}
 
-function hideButtons() {
-    $('#site_choice').hide();
-}
+//function showDrawing() {
+//    $('#site_drawing').show();
+//}
 
-function click_delete() {
-    $('.node_tree').bind('click', function () {
-        jsPlumb.detachAllConnections(this.id);
-        jsPlumb.removeAllEndpoints(this.id);
-        $(this).remove();
-    });
+//function hideButtons() {
+//    $('#site_choice').hide();
+//}
 
-    $('.canvas').bind('click', function () {
-        $('.node_tree').unbind();
-    });
+//function click_delete() {
+//    $('.node_tree').bind('click', function () {
+//        jsPlumb.detachAllConnections(this.id);
+//        jsPlumb.removeAllEndpoints(this.id);
+//        $(this).remove();
+//    });
+
+//    $('.canvas').bind('click', function () {
+//        $('.node_tree').unbind();
+//    });
 
 
-}
+//}
